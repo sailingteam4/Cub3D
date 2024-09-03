b14e9cb36d0be7de6f559f1429cd6ed08297ee99
sailingteam4 <sailing4team@gmail.com>
2024-09-03 20:31:36 +0200

    [CHANGE] little rework of check file name func to take less lines

--- src/str/parsing_utils.c
+++ src/str/parsing_utils.c
@@ -6,7 +6,7 @@
 /*   By: nrontey <nrontey@student.42angouleme.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/03 19:11:02 by nrontey           #+#    #+#             */
-/*   Updated: 2024/09/03 20:26:04 by nrontey          ###   ########.fr       */
+/*   Updated: 2024/09/03 20:30:48 by nrontey          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,9 @@
 
 bool	check_file_name(char *filename)
 {
-	if (!filename || ft_strlen(filename) < 4)
+	if ((!filename || ft_strlen(filename) < 4) || (ft_strcmp(filename + ft_strlen(filename) - 4, ".cub")))
 	{
-		printf("Error\nInvalid file name\n");
-		return (false);
-	}
-	if (ft_strcmp(filename + ft_strlen(filename) - 4, ".cub"))
-	{
-		printf("Error\nInvalid file extension\n");
+		printf("Error\nInvalid file name or extension\n");
 		return (false);
 	}
 	return (true);
