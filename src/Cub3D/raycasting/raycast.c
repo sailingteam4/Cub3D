18be5334cf1b27305aa5e126984c50c5d7e242a8
Tristan Pipi <tpipi@2B1.42angouleme.fr>
2024-12-17 00:00:14 +0100

    fix

--- src/Cub3D/raycasting/raycast.c
+++ src/Cub3D/raycasting/raycast.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   raycast.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nrontey <nrontey@student.42angouleme.fr    +#+  +:+       +#+        */
+/*   By: tpipi <tpipi@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/16 20:44:43 by tpipi             #+#    #+#             */
-/*   Updated: 2024/12/16 23:36:28 by nrontey          ###   ########.fr       */
+/*   Updated: 2024/12/16 23:58:27 by tpipi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,8 +28,8 @@ void	get_horizontal_value(t_raycast *rc, float angle, float x, float y)
 		rc->offset_x = -rc->offset_y * -1/tan(angle);
 	}
 	else {
-		rc->nearest_y = y;
-		rc->nearest_x = x;
+		rc->nearest_y = 2147483647;
+		rc->nearest_x = 2147483647;
 		rc->offset_x = 0;
 		rc->offset_y = 0;
 		rc->hit = 0;
@@ -52,22 +52,22 @@ void	get_vertical_value(t_raycast *rc, float angle, float x, float y)
 	    rc->offset_y = rc->offset_x * -tan(angle);
 	}
 	else {
-	    rc->nearest_y = y;
-	    rc->nearest_x = x;
+	    rc->nearest_y = 2147483647;
+	    rc->nearest_x = 2147483647;
 		rc->offset_x = 0;
 		rc->offset_y = 0;
 	    rc->hit = 0;
 	}
 }
 
-void	find_wall(t_raycast *rc, float x, float y, t_map *map)
+void	find_wall(t_raycast *rc, t_map *map)
 {
 	while (rc->hit) {
 		if (rc->nearest_x < 0 || rc->nearest_x >= map->map_width || \
 			rc->nearest_y < 0 || rc->nearest_y >= map->map_height)
 		{
-			rc->nearest_y = y;
-			rc->nearest_x = x;
+			rc->nearest_y = 2147483647;
+			rc->nearest_x = 2147483647;
 			rc->hit = 0;
 		} 
 		else if (map->map_2d[(int)rc->nearest_y][(int)rc->nearest_x] == '1')
@@ -80,7 +80,7 @@ void	find_wall(t_raycast *rc, float x, float y, t_map *map)
 	}
 }
 #include <stdio.h>
-#include <float.h>
+
 t_raycast ft_raycaster(t_map *map, t_player *player, float angle)
 {
 	t_raycast	rc;
@@ -89,25 +89,15 @@ t_raycast ft_raycaster(t_map *map, t_player *player, float angle)
 
 	x = player->current_position->y;
 	y = player->current_position->x;
-	rc.wall_x = 0;
-	rc.wall_y = 0;
-	rc.dist = FLT_MAX;
 	get_horizontal_value(&rc, angle, x, y);
-	find_wall(&rc, x, y, map);
-	if (rc.nearest_x != x && rc.nearest_y != y)
-	{
-		rc.wall_x = (int)rc.nearest_x;
-		rc.wall_y = (int)rc.nearest_y;
-		rc.dist = sqrt((rc.nearest_x-x)*(rc.nearest_x-x)+(rc.nearest_y-y)*(rc.nearest_y-y));
-	}
+	find_wall(&rc, map);
+	rc.wall_x = (int)rc.nearest_x;
+	rc.wall_y = (int)rc.nearest_y;
+	rc.dist = sqrt((rc.nearest_x-x)*(rc.nearest_x-x)+(rc.nearest_y-y)*(rc.nearest_y-y));
+	printf("Horizontal Wall Values x:%d y:%d\n", rc.wall_x, rc.wall_y);
 	get_vertical_value(&rc, angle, x, y);
-	find_wall(&rc, x, y, map);
-	if (rc.nearest_x != x && rc.nearest_y != y)
-	{
-		rc.wall_x = (int)rc.nearest_x;
-		rc.wall_y = (int)rc.nearest_y;
-		rc.dist = sqrt((rc.nearest_x-x)*(rc.nearest_x-x)+(rc.nearest_y-y)*(rc.nearest_y-y));
-	}
+	find_wall(&rc, map);
+	printf("Vertical Wall Values x:%d y:%d\n", (int)rc.nearest_x, (int)rc.nearest_y);
 	if (sqrt((rc.nearest_x-x)*(rc.nearest_x-x)+(rc.nearest_y-y)*(rc.nearest_y-y)) < rc.dist)
 	{
 		rc.wall_x = (int)rc.nearest_x;
